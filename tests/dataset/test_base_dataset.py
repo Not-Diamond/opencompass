1df44307ec2196a8000d0708c238ac249f693864
Alejandro Companioni <a9@notdiamond.ai>
2024-05-04 03:33:45 +0000

    initial failing test

---
+++ tests/dataset/test_base_dataset.py
@@ -0,0 +1,12 @@
+from opencompass.datasets.base import BaseDataset
+
+
+def test_base_dataset_build_denylist():
+    # Create mock or temp dir structure
+
+    # Add json file for two models - one has a denylist, the other does not
+
+    # Call build_denylist for the dataset with the denylist
+
+    # Ensure the denylist is returned
+    assert False
