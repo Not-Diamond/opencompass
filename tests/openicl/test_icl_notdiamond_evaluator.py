8a6a74b8cd087ffad1a0f9d02679b9549b31d650
Alejandro Companioni <a9@notdiamond.ai>
2024-05-06 02:26:22 +0000

    unit tests

---
+++ tests/openicl/test_icl_notdiamond_evaluator.py
@@ -0,0 +1,9 @@
+import pytest
+
+from opencompass.opencompass.openicl.icl_evaluator.icl_notdiamond_evaluator import (
+    NDMgsmEvaluator,
+)
+
+
+def test_mgsm_evaluator(mgsm_example):
+    assert False
