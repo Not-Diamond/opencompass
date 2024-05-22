51ef009690725a0e89d47edde8e54bc469ff1891
Alejandro Companioni <a9@notdiamond.ai>
2024-05-22 00:50:03 +0000

    Adding NDNaivePartitioner to shuffle tasks.

--- opencompass/partitioners/__init__.py
+++ opencompass/partitioners/__init__.py
@@ -2,3 +2,4 @@ from .mm_naive import *  # noqa: F401, F403
 from .naive import *  # noqa: F401, F403
 from .num_worker import *  # noqa: F401, F403
 from .size import *  # noqa: F401, F403
+from .nd_naive import *
